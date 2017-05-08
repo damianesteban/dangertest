fb4564939e26b05c33bcf1de422971a6fa417279
Damian Esteban <damian.esteban@gmail.com>
2017-05-08 19:58:29 -0400

    Initial commit

---
+++ DangerTest/Arithmetic.swift
@@ -0,0 +1,28 @@
+//
+//  Addition.swift
+//  DangerTest
+//
+//  Created by Damian Esteban on 5/8/17.
+//  Copyright © 2017 Damian Esteban. All rights reserved.
+//
+
+import Foundation
+
+struct Arithmetic {
+    func add(_ x: Int, _ y: Int) -> Int {
+        return x + y
+    }
+    
+    func multiply(_ x: Int, _ y: Int) -> Int {
+        return x * y
+    }
+    
+    func subtract (_ x: Int, _ y: Int) -> Int {
+        guard x > y else {
+            return 0
+        }
+        return x - y
+    }
+    
+    
+}
---
+++ DangerTestTests/DangerTestTests.swift
@@ -0,0 +1,38 @@
+//
+//  DangerTestTests.swift
+//  DangerTestTests
+//
+//  Created by Damian Esteban on 5/8/17.
+//  Copyright © 2017 Damian Esteban. All rights reserved.
+//
+
+import XCTest
+@testable import DangerTest
+
+class DangerTestTests: XCTestCase {
+    
+    let sut = Arithmetic()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testAddition() {
+        XCTAssertEqual(sut.add(7, 7), 14, "performs addition")
+    }
+    
+    func testSubtraction() {
+        XCTAssertEqual(sut.subtract(10, 5), 5, "performs subtraction")
+    }
+    
+    func testMultiplication() {
+        XCTAssertEqual(sut.multiply(10, 5), 50, "performs subtraction")
+    }
+    
+}
